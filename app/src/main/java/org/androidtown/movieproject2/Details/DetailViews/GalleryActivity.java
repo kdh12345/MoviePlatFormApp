4df3162323450bf96d45bf11ec3f369e7c702838
kdh123456 <go3572@naver.com>
2020-04-15 19:55:12 +0900

    8stage End

--- app/src/main/java/org/androidtown/movieproject2/Details/DetailViews/GalleryActivity.java
+++ app/src/main/java/org/androidtown/movieproject2/Details/DetailViews/GalleryActivity.java
@@ -1,10 +1,12 @@
 package org.androidtown.movieproject2.Details.DetailViews;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
 import com.github.chrisbanes.photoview.PhotoView;
@@ -12,16 +14,26 @@ import com.github.chrisbanes.photoview.PhotoView;
 import org.androidtown.movieproject2.R;
 
 public class GalleryActivity extends AppCompatActivity {
-    ActionBar actionBar;
+    Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-        actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
+        toolbar = (Toolbar) findViewById(R.id.gallery_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("영화 사진");
         String photoUrl = getIntent().getStringExtra("url");
         PhotoView photoView = findViewById(R.id.photoview);
         Glide.with(getApplicationContext()).load(photoUrl).into(photoView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
